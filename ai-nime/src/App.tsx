63d7dad8d66dc90b66d85c99e99230423d9511db
daniel32581 <daniel.hong.785@gmail.com>
2026-01-17 13:36:47 -0500

    start search bar

--- ai-nime/src/App.tsx
+++ ai-nime/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import style from './App.module.css'
+import SearchBarComponent from './lib/Searchbar.jsx'
 
 function App() {
-  
+  return<SearchBarComponent />;
 }
 
 export default App
